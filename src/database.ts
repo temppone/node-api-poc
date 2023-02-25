6d42c20d36eda36afb451b6138aa94b048840b0f
Gustavo Tempone de Oliveira Faria <gustavo.faria@efrete.com.br>
2023-02-25 00:32:37 -0300

    feat: initial config and transactions routes

---
+++ src/database.ts
@@ -0,0 +1,19 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { knex as setupKnex, Knex } from 'knex';
+import { env } from './env';
+
+export const config: Knex.Config = {
+  client: 'sqlite',
+  connection: {
+    filename: env.DATABASE_URL,
+  },
+
+  migrations: {
+    extension: 'ts',
+    directory: './db/migrations',
+  },
+
+  useNullAsDefault: true,
+};
+
+export const knex = setupKnex(config);
