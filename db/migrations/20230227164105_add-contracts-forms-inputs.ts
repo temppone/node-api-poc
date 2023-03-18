0419d8729314dc42003de699a175d39af7df777d
Gustavo Tempone de Oliveira Faria <gustavo.faria@efrete.com.br>
2023-03-18 10:40:49 -0300

    fix: migrate table name to contractsForms

--- db/migrations/20230227164105_add-contracts-forms-inputs.ts
+++ db/migrations/20230227164105_add-contracts-forms-inputs.ts
@@ -7,7 +7,7 @@ export async function up(knex: Knex): Promise<void> {
     table.string('type').notNullable();
     table.string('question_label').notNullable();
     table.string('contract_type_id').notNullable();
-    table.foreign('contract_type_id').references('id').inTable('contractsType');
+    table.foreign('contract_type_id').references('id').inTable('contractsForms');
   });
 }
 
