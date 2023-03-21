d85b1ba3e2328f24222f03ba3383c13ddab22dac
Gustavo Tempone de Oliveira Faria <gustavo.faria@efrete.com.br>
2023-03-21 13:04:20 -0300

    chore: remove transactions

--- src/@types/knex.d.ts
+++ src/@types/knex.d.ts
@@ -3,14 +3,6 @@ import { knex } from 'knex';
 
 declare module 'knex/types/tables' {
   export interface Tables {
-    transactions: {
-      id: string;
-      title: string;
-      amount: number;
-      created_at: string;
-      session_id?: string;
-    };
-
     contractsForms: {
       id: string;
       type: string;
